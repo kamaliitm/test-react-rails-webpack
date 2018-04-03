37cf7d0849c8a67fa8e69fb09f564266b8979e45
Kamal Varma Kambe <kamal.kambe@rockyou.com>
2018-04-03 18:23:03 +0530

    Added thumbnails

--- app/javascript/components/Home/Tweet.js
+++ app/javascript/components/Home/Tweet.js
@@ -3,12 +3,13 @@ import PropTypes from "prop-types"
 
 class Tweet extends React.Component {
   render () {
-    console.log("Tweet: Tweet Obj: ", this.props.tweet);
     return (
       <div className="media">
         <img className="mr-3" src={this.props.tweet.profile_image_url} alt="Profile Pic Thumbnail" />
-        <p><u>{this.props.tweet.name}</u></p>
-        <p>{this.props.tweet.text}</p>
+        <div className="media-body">
+          <h5 className="mt-0"><u>{this.props.tweet.name}</u></h5>
+          <p>{this.props.tweet.text}</p>
+        </div>
         <hr/>
       </div>
     )
