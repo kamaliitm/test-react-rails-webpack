742dc3cddd3fa308416e7ef299a32f44ff802be5
Kamal Varma Kambe <kamal.kambe@rockyou.com>
2018-04-04 18:13:18 +0530

    Added bootstrap card to tweets in timeline

--- app/javascript/components/Home/Tweet.js
+++ app/javascript/components/Home/Tweet.js
@@ -4,13 +4,15 @@ import PropTypes from "prop-types"
 class Tweet extends React.Component {
   render () {
     return (
-      <div className="media">
-        <img className="mr-3" src={this.props.tweet.profile_image_url} alt="Profile Pic Thumbnail" />
-        <div className="media-body">
-          <h5 className="mt-0"><u>{this.props.tweet.name}</u></h5>
-          <p>{this.props.tweet.text}</p>
+      <div className="tweet card card-block bg-faded">
+        <div className="media">
+          <img className="mr-3" src={this.props.tweet.profile_image_url} alt="Profile Pic Thumbnail" />
+          <div className="media-body">
+            <h5 className="mt-0"><u>{this.props.tweet.name}</u></h5>
+            {this.props.tweet.text}
+          </div>
+          <hr/>
         </div>
-        <hr/>
       </div>
     )
   }
